b0b3dbba4b78f2c462bcf5256512336919964ca4
roleskaber <roleskabersupp@gmail.com>
2025-03-24 23:07:14 +0300

    Ainmation testing

---
+++ Sphera 1.2/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Sphera 1.2
+//
+//  Created by Виталик Марченко on 24.03.25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
